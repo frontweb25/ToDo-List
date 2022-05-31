93586d4bfd4a87619e23b159a40bee90ce31174b
Zhenya <ruban.z1@mail.ru>
2022-05-31 21:53:35 +0300

    Функция добавления задачи

--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
 
-const dom = {}
+const dom = {
+    new: document.querySelector('#new'),
+    add: document.querySelector('#add'),
+    tasks: document.querySelector('#tasks')
+};
+
+const arrTasks = [];
+
+//Отслеживаем клик по кнопке для добавления задачи
+
+dom.add.addEventListener('click', () => {
+    const task = dom.new.value;
+    if(task) {
+        addTask(task); 
+        dom.new.value = '';
+    } 
+    
+    
+});
+
+//Функция добавления задачи
+function addTask(text) {
+    const timestamp = Date.now();
+    const task = {
+        id: timestamp,
+        text: text,
+        isComplete: false,
+    };
+    arrTasks.push(task);
+    console.log(arrTasks)
+}
+
+
